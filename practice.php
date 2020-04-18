f40b0f99615047b7727f35191840b7f9d0607658
”onozawa-kazuya” <kazuya.jpee@gmail.com>
2020-04-18 07:40:59 +0000

    task1

---
+++ practice.php
@@ -0,0 +1,39 @@
+<?php
+
+// 課題１
+$a = 3;
+$b = 7;
+
+echo $a +$b.PHP_EOL;
+
+
+
+
+// 課題２
+$array_month = ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'];
+
+echo $array_month[7].PHP_EOL;
+
+
+
+// 課題３
+$hello = "Hello, ";
+$name = "onozawa";
+$word = "'s world!";
+
+echo $hello.$name.$word.PHP_EOL;
+
+
+
+// 課題４
+$tech_boost = 'tech';
+$tech_boost .= ' boost';
+
+echo $tech_boost;
+
+
+
+// 課題５
+// 間違いは5つです
+
+
